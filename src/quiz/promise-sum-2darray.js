dc27678491515b68f9800db36d42b780a304e0bc
Mo Barre <mbarre95@gmail.com>
2024-02-29 18:56:49 -0500

    Feb 29 updates

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,19 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+
+// rowSumPromises = [];
+
+// for (let x=0; x<array2D.length; x++) {
+//     rowSumPromises.push(sumOfARow(array2D. x));
+// }
+
+// Promise.all(rowSumPromises)
+//     .then((rowSums)) =>
+//         let sum = 0;
+//         rowSums.forEach(rowSum => {
+//             sum += rowSum;
+//         })
+//         console.log(`Sum = ${sum}`);
+//     .catch((error)) => console.log(`Error Msg: ${error}`);
